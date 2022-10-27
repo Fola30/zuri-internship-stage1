ac22a0563f0d7524ef600f4f8225d9a221969824
AAAAA <AAAAA@DESKTOP-M0J6QD5.local>
2022-10-27 19:32:53 +0100

    Final commit

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+
+@app.route('/info')
+def info():
     return jsonify(slackUsername='Fola23', backend=True, age=19, bio="My name is Rasheed, i am a backend developer "
                                                                      "who's always excited to learn new things.")
 
