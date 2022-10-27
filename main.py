81eab5b28891ce2b0acc00a23649ca59267ac8ed
AAAAA <AAAAA@DESKTOP-M0J6QD5.local>
2022-10-27 19:01:06 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return jsonify(slackUsername='Fola23', backend=True, age=19, bio="My name is Rasheed, i am a backend developer "
+                                                                     "who's always excited to learn new things.")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
