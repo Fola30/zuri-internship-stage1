c1ae583e904799f9535ca7cf42de5289ab5aff71
AAAAA <AAAAA@DESKTOP-M0J6QD5.local>
2022-10-29 16:10:29 +0100

    Final commit

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return jsonify(slackUsername='Fola23', backend=True, age=19, bio="My name is Rasheed, i am a backend developer "
-                                                                     "who's always excited to learn new things...")
+    data = jsonify(slackUsername='Fola23', backend=True, age=19, bio="My name is Rasheed, i am a backend developer "
+                                                                     "who's always excited to learn new things...").json
+
+    return render_template('index.html', data=data)
 
 
 if __name__ == '__main__':
